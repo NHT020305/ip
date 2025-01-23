c6bae7ee082afae8fcb01333c408d80b73c7ceae
NHT020305 <tungtun020305@gmail.com>
2025-01-23 22:10:51 +0700

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,6 +25,7 @@ public class Duke {
                     break;
                 } else if (command.equals("list")) {
                     System.out.println("____________________________________________________________");
+                    System.out.println(" Here are the tasks in your list:");
                     for (int i = 0; i < tasks.size(); i++) {
                         System.out.println((i + 1) + "." + tasks.get(i));
                     }
