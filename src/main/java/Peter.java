07abc18d66e7a72a76759cc33f36bcb5da65b587
NHT020305 <tungtun020305@gmail.com>
2025-01-23 23:20:20 +0700

    update

--- src/main/java/Peter.java
+++ src/main/java/Peter.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-public class Duke {
+public class Peter {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
 
