8e383f1229e19e9d414981bc88cd2bb34ed9db64
NHT020305 <tungtun020305@gmail.com>
2025-02-12 03:26:26 +0800

    Update project

--- src/main/java/peter/Main.java
+++ src/main/java/peter/Main.java
@@ -26,7 +26,7 @@ public class Main extends Application {
             stage.show();
             controller.setPeter();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 }
